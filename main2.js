bac90f1ef90a9f36f6f07e1257e89e96a0e61cac
Ivy Vo <ivyvodesign@gmail.com>
2020-05-13 12:07:34 +0700

    add more currencies

--- main2.js
+++ main2.js
@@ -1,29 +1,32 @@
 // exchange rate
-const exchangeRate = 23333.5;
+//const exchangeRate = 23333.5;
+
+  
+
 let from = document.getElementById('fromList')
 let to = document.getElementById('toList')
 let convertButton = document.getElementById('convertButton');
 let amount = document.getElementById('amountInput')
 let result = document.getElementById('resultText');
 let convertedAmount = 0;
-let formatedAmount = ' '
+let formatedAmount = ''
 
 
 convertButton.addEventListener("click",convert);
 
-// VND to USD
+// // VND to USD
 
-function vndToUsd() {
-  return amount.value/ exchangeRate.toFixed(2)
-  //console.log("your money in USD is ", convertedAmount);
-}
+// function vndToUsd() {
+//   return amount.value/ exchangeRate.toFixed(2)
+//   //console.log("your money in USD is ", convertedAmount);
+// }
 
-//USD to VND
+// //USD to VND
 
-function usdToVnd(){
-  return amount.value* exchangeRate.toFixed(2)
-  //console.log("your money in VND is", convertedAmount);
-}
+// function usdToVnd(){
+//   return amount.value* exchangeRate.toFixed(2)
+//   //console.log("your money in VND is", convertedAmount);
+// }
 
 // formatting currency
 
@@ -36,23 +39,61 @@ function formatCurrency(type, value) {
 }
 
 
-function convert (){
+// function convert (){
   
  
-   if (from.value === 'VND' && to.value === 'USD'){
-     convertedAmount = vndToUsd()
-     formatedAmount = formatCurrency(to.value,convertedAmount)
+//    if (from.value === 'VND' && to.value === 'USD'){
+//      convertedAmount = vndToUsd()
+//      formatedAmount = formatCurrency(to.value,convertedAmount)
      
-   } else if (from.value === 'USD' && to.value === 'VND'){
-     convertedAmount = usdToVnd()
-     formatedAmount = formatCurrency(to.value,convertedAmount)
+//    } else if (from.value === 'USD' && to.value === 'VND'){
+//      convertedAmount = usdToVnd()
+//      formatedAmount = formatCurrency(to.value,convertedAmount)
    
-   } else {
-     result.innerHTML = `Choose different converting currency`
-     return; 
-   }
-   result.innerHTML = `You have ${formatedAmount}`
- }
+//    } else {
+//      result.innerHTML = `Choose different converting currency`
+//      return; 
+//    }
+//    result.innerHTML = `You have ${formatedAmount}`
+//  }
  
 
- 
+const currencyRatio = {
+  VND:{
+    USD: 0.000043,
+    KRW: 0.051,
+    GBP: 0.0000349,
+    VND: 1
+  },
+  USD: {
+    USD: 1,
+    KRW: 1225.68,
+    GBP: 0.815202,
+    VND: 23333.5
+  },
+  KRW: {
+    USD: 0.00081590,
+    KRW: 1,
+    GBP: 0.000665130,
+    VND: 19.47
+  },
+  GBP:{
+    USB: 1.22673,
+    KRW: 1503.47,
+    GBP: 1,
+    VND: 28615.8
+  }
+
+};
+
+function getConversionRate(from,to) {
+  return currencyRatio[from.value][to.value];
+}
+
+
+function convert(){
+  convertedAmount = amount.value* currencyRatio[from.value][to.value]
+  formatedAmount = formatCurrency(to.value,convertedAmount)
+  result.innerHTML = `You have ${formatedAmount}`
+
+}
